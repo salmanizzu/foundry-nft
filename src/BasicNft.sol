1bc0340b0d5868f2539cd2f580346081dafb25ce
Salman Izzuddin <salmanizzu.dev@protonmail.com>
2025-03-01 02:01:06 +0700

    contract: add tests and interactions

--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -19,9 +19,7 @@ contract BasicNft is ERC721 {
         s_tokenCounter = s_tokenCounter + 1;
     }
 
-    function tokenURI(
-        uint256 tokenId
-    ) public view override returns (string memory) {
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
         return s_tokenIdToUri[tokenId];
     }
 }
---
+++ test/BasicNftTest.t.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import {DeployBasicNft} from "../script/DeployBasicNft.s.sol";
+import {BasicNft} from "../src/BasicNft.sol";
+import {Test, console2} from "forge-std/Test.sol";
+
+contract BasicNftTest is Test {
+    string constant NFT_NAME = "Dogie";
+    string constant NFT_SYMBOL = "DOG";
+    BasicNft public basicNft;
+    DeployBasicNft public deployer;
+    address public constant USER = address(1);
+    string public constant PUG_URI =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+
+    function setUp() public {
+        deployer = new DeployBasicNft();
+        basicNft = deployer.run();
+    }
+
+    function testInitializedCorrectly() public view {
+        assert(keccak256(abi.encodePacked(basicNft.name())) == keccak256(abi.encodePacked((NFT_NAME))));
+        assert(keccak256(abi.encodePacked(basicNft.symbol())) == keccak256(abi.encodePacked((NFT_SYMBOL))));
+    }
+
+    function testCanMintAndHaveABalance() public {
+        vm.prank(USER);
+        basicNft.mintNft(PUG_URI);
+
+        assert(basicNft.balanceOf(USER) == 1);
+    }
+}
